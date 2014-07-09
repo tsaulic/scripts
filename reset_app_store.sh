2a63bf036d82046fe5e95c40f10fad48bf2efbf0
Tihomir Saulic <Tihomir.Saulic@wotifgroup.com>
2014-07-09 14:32:53 +1000

    Initial commit.

---
+++ reset_app_store.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm ~/Library/Preferences/com.apple.appstore.plist
+rm -r ~/Library/Saved\ Application\ State/com.apple.appstore.savedState
+rm -r ~/Library/Caches/com.apple.appstore
+rm -r /private/var/folders/*/*/*/com.apple.appstore
