08bb79d0dbf4617e0f4ce3d62679c8a24cb42d14
Tihomir Saulic <tsaulic@wotifgroup.com>
2015-01-20 13:18:16 +1000

    Improve largest_files.sh script. Now excludes revision control system folders (Git, SVN and mercurial)

--- largest_files.sh
+++ largest_files.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-find . -type f -exec ls -s {} \; | sort -n -r | head -10
+# finds the largest files in a directory, excluding .git, .svn and .hg folders
+
+find . -type f -not -iwholename '*.git*' -not -iwholename '*.svn*' \
+-not -iwholename '*.hg*' -exec ls -s {} \; | sort -n -r | head -10
