fc30648891cd2ed8c568ecdf1a6e4f0b45be8c22
Tihomir Saulic <tsaulic@wotifgroup.com>
2015-01-21 09:50:16 +1000

    Added a script which locates the largest text files only.

---
+++ largest_text_files.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# finds the largest text files in a directory, excluding .git, .svn
+# and .hg folders
+
+find . -type f -not -iwholename '*.git*' -not -iwholename '*.svn*' \
+-exec file {} \; | grep text | cut -d: -f1 | xargs ls -s | \
+sort -n -r | head -15
